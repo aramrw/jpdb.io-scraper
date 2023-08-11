938e059f3700376834da00021b4fea3153ae42ce
aramrw <106574385+aramrw@users.noreply.github.com>
2023-08-11 16:40:25 -0700

    Update scraper.js
    Added : 
    - links to any novel + anime (finished)
    - offset modifier
    - write to a textfile (not done)

--- scraper.js
+++ scraper.js
@@ -1,6 +1,8 @@
+const { readlink } = require('fs');
 const puppeteer = require('puppeteer');
 const readline = require('readline');
 const { arrayBuffer } = require('stream/consumers');
+const fs = require('fs');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -15,11 +17,8 @@ const rl = readline.createInterface({
     rl.question('Please enter the novel or anime title: ', async (searchQuery) => {
         // Navigate to jpdb.io
         await page.goto('https://jpdb.io/prebuilt_decks');
-        
 
         // Input the user's search query
-        //await page.$x('/html/body/div[2]/form/input[1]', searchQuery);
-
         await page.type('input[name="q"]', searchQuery);
 
         // Click the search button
@@ -32,36 +31,111 @@ const rl = readline.createInterface({
         const animeNames = await page.evaluate(() => {
             const nameElements = document.querySelectorAll('h5');
             return Array.from(nameElements, element => element.textContent.trim());
-        })
+        });
 
-        // Check anime name 
-        // console.log(`Checking jpdb.io for ${searchQuery} against ${animeNames}...`); 
-
+        // Check anime name
         const lowerCaseSearchQuery = searchQuery.toLowerCase();
+        const matchingAnime = [];
 
         let animeFound = false;
-        let animeAmount =0;
+        let animeAmount = 0;
 
         for (const animeName of animeNames) {
-            
             const lowerCaseAnimeNames = animeName.toLowerCase();
             let includesTitle = lowerCaseAnimeNames.includes(lowerCaseSearchQuery);
-
-            if(includesTitle) {
+            if (includesTitle) {
                 animeFound = true;
-                animeAmount++;
-        }}
-
-        if(animeFound) {
-            console.log(` ${animeAmount} Anime including "${searchQuery}" have been FOUND.`)
-        }else{
-            console.log(`0 Anime including "${searchQuery}" have been found.`);
+                matchingAnime.push(animeName);
+            }
         }
-       
 
-        // Exit the script
-        await browser.close();
-        process.exit(); 
-        
+        if (animeFound) {
+            console.log(` ${matchingAnime.length} Anime including "${searchQuery}" have been FOUND.\n`);
+
+            //display matching anime names
+            matchingAnime.forEach((anime, index) => {
+                console.log(`${index + 1}: ${anime}`);
+            })
+
+            // pick the correct anime
+            rl.question('Select an anime by entering its number: \n', async (selection) => {
+                const selectedIndex = parseInt(selection) - 1;
+                if (selectedIndex >= 0 && selectedIndex < matchingAnime.length) {
+                    const selectedAnime = matchingAnime[selectedIndex];
+                    console.log(`You selected: ${selectedAnime}.\n`);
+
+                    // Wait for anchor links 
+                    await page.waitForXPath('/html/body/div/div/div/div/div/a');
+
+                    // Get the list of anchor links
+                    const anchorLinks = await page.evaluate(() => {
+                        const nameElements = document.querySelectorAll('a');
+                        return Array.from(nameElements, element => element.getAttribute('href'));
+                    });
+
+                    let anchorLinkFound = false;
+                    let skipCounter = 0;
+                    const modifiedSelectedAnime = selectedAnime.replace(':', '').split(' ').join('-').toLowerCase();
+
+                    for (const anchorLink of anchorLinks) {
+                            if(!(anchorLink.includes("/stats")) && anchorLink.includes(modifiedSelectedAnime)){
+                                skipCounter++;
+
+                                // console.log(anchorLink); Check anchor url
+                                if (anchorLink.includes(modifiedSelectedAnime)) {
+                                    anchorLinkFound = true;
+
+                                    rl.question('Please enter search offset: ', async (vocabOffset) => {
+                                        vocabListLink = anchorLink.toString() + `/vocabulary-list?offset=${vocabOffset}&sort_by=by-frequency-global`;
+                                        await page.goto(`https://jpdb.io${vocabListLink}`);
+                                        console.log(`Frequency link of ${selectedAnime} found.\n`);
+                                        
+                                        // TODO: scrape frequency data
+                                    await page.waitForXPath('//ruby');
+
+                                        // Get list of ruby words
+                                        const rubyWords = await page.evaluate(() => {
+                                        const nameElements = document.querySelectorAll('ruby');
+                                        return Array.from(nameElements, element => element.textContent.trim());
+                                    });
+                                        if(rubyWords == "" || rubyWords == null){
+                                            console.log(`Invalid offset amount: ${rubyWords}`);
+                                        }else{
+                                             // Write rubyWords to a text file
+                                            const outputFilePath = "ruby_words.txt"
+                                            fs.writeFileSync(outputFilePath, rubyWords.join('\n'), { encoding: 'utf-8' });
+                                            
+
+                                            console.log(`Ruby words written to ${outputFilePath}`);
+                                        }
+                                       
+                                    })
+                                    
+                                }else{
+                                    if(anchorLinkFound = false){
+                                        console.log(`Anchor link for ${modifiedSelectedAnime} not found!\n`);
+                                        break;
+                                    }
+                                }
+                            }else{
+                                if(anchorLinkFound = false){
+                                    console.log(`Anchor link for ${modifiedSelectedAnime} not found!\n`);
+                                    break;
+                                }
+                            }
+                       
+                    }
+                        // TODO: scrape frequency data
+                } else {
+                    console.log('Invalid Selection.\n');
+                }
+            });
+
+        } else {
+            console.log(`0 Anime including "${searchQuery}" have been found. \n`);
+            await browser.close();
+            process.exit();
+        }
+
     });
 })();
